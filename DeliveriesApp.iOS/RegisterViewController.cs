9b834e7015d439363bb84148b22dca051b186922
Peter N Jenkin <peternjenkin@gmail.com>
2019-06-13 16:08:55 +0100

    16-139 ViewController class added for Register page

---
+++ DeliveriesApp.iOS/RegisterViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace DeliveriesApp.iOS
+{
+    public partial class RegisterViewController : UIView
+    {
+        public RegisterViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
