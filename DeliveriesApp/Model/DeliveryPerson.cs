5956ab57c4224f96d92aed46a54e0587c5ee7a7d
Peter N Jenkin <peternjenkin@gmail.com>
2019-06-22 21:40:43 +0100

    18-150 Code-first classes for Delivery and DeliveryPerson, with GetDeliveries and GetDeliveryPerson methods

---
+++ DeliveriesApp/Model/DeliveryPerson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveriesApp.Model
+{
+    class DeliveryPerson
+    {
+
+        public string Id { get; set; }
+
+        public static async Task<DeliveryPerson> GetDeliveryPerson(string id)
+        {
+            DeliveryPerson person = new DeliveryPerson();
+
+            person = (await AzureHelper.MobileService.GetTable<DeliveryPerson>().Where(dp => dp.Id == id).ToListAsync()).FirstOrDefault();
+            // NB pattern for single instances (eg an individual person) - brackets and ().FirstOrDefault
+
+            return person;
+        }
+    }
+}
