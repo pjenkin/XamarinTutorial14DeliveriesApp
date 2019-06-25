0ba9adc1a44532ff258c701057a45ae97d3ce385
Peter N Jenkin <peternjenkin@gmail.com>
2019-06-25 13:19:18 +0100

    19-155 Android fragments for Deliveries, Delivered & Profile added, with FragmentActvity classes to match, so that in TabLayout these pages can be navigated by the user

---
+++ DeliveriesApp.Droid/DeliveriesFragment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace DeliveriesApp.Droid
+{
+    // public class DeliveriesFragment : Fragment
+    public class DeliveriesFragment : Android.Support.V4.App.Fragment       // derive from support v4 for backward-compatibility
+    {
+            public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            return inflater.Inflate(Resource.Layout.Deliveries, container, false);
+            // Un-comment & amend the above auto-commented boilerplate to inflate our Deliveries fragment layout
+
+            return base.OnCreateView(inflater, container, savedInstanceState);
+        }
+    }
+}
--- DeliveriesApp.Droid/TabsActivity.cs
+++ DeliveriesApp.Droid/TabsActivity.cs
@@ -7,14 +7,19 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.App;
 using Android.Views;
 using Android.Widget;
 
 namespace DeliveriesApp.Droid
 {
     [Activity(Label = "TabsActivity")]
-    public class TabsActivity : Activity
+    //public class TabsActivity : Activity
+    public class TabsActivity : FragmentActivity            // must be a FragmentActivity for navigation - SupportFragmentManager
     {
+        TabLayout tabLayout;      // refer to our new 'main' layout
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -22,6 +27,38 @@ namespace DeliveriesApp.Droid
             // Create your application here
             // ok! :-) This activity will be to do with our Tabs layout (ie the new main page)
             SetContentView(Resource.Layout.Tabs);
+
+            tabLayout = this.FindViewById<TabLayout>(Resource.Id.mainTabLayout);  // get our tab by id defined as android:id="@+id/mainTabLayout"
+            tabLayout.TabSelected += TabLayout_TabSelected;                         // += then TAB for auto-complete of event handler
+
+            FragmentNavigate(new DeliveriesFragment());     // in effect, make Deliveries the default fragment on startup
         }
+
+        private void TabLayout_TabSelected(object sender, TabLayout.TabSelectedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            // NB position (ie 1st tabItem, 2nd TabItem, ...), instead of id, needed for determining which tab was selected. Thus in 19-155 no ids defined for Tab Items.
+            switch(e.Tab.Position)
+            {
+                case 0:
+                    FragmentNavigate(new DeliveriesFragment());
+                    break;
+                case 1:
+                    FragmentNavigate(new DeliveredFragment());
+                    break;
+                case 2:
+                    FragmentNavigate(new ProfileFragment());
+                    break;
+            }
+        }
+
+        /// bespoke DRY method for navigating to selected fragment layout
+        private void FragmentNavigate(Android.Support.V4.App.Fragment fragment)
+        {
+            var transaction = SupportFragmentManager.BeginTransaction();    // could reset, replace, remove &c fragments
+            transaction.Replace(Resource.Id.contentFrame, fragment);
+            transaction.Commit();
+        }
+
     }
 }
