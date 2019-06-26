b4ea31d8bd189e480002b2d6e51f0c19aead5fe3
Peter N Jenkin <peternjenkin@gmail.com>
2019-06-26 14:19:15 +0100

    20-160 3 TabViewControllers added with icons in iOS

---
+++ DeliveriesApp.iOS/DeliveredViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace DeliveriesApp.iOS
+{
+    public partial class DeliveredViewController : UITableViewController
+    {
+        public DeliveredViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ DeliveriesApp.iOS/ProfileViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace DeliveriesApp.iOS
+{
+    public partial class ProfileViewController : UITableViewController
+    {
+        public ProfileViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
