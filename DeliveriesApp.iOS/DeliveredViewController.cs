aa10e40a098d706b0ff501d0cb87e735e3b12ba0
Peter N Jenkin <peternjenkin@gmail.com>
2019-06-26 19:28:22 +0100

    22-166 Added a list of Delivery data objects as a member of the class and populate that member in ViewDidLoad, for Android

--- DeliveriesApp.iOS/DeliveredViewController.cs
+++ DeliveriesApp.iOS/DeliveredViewController.cs
@@ -1,13 +1,24 @@
+using DeliveriesApp.Model;
 using Foundation;
 using System;
+using System.Collections.Generic;
 using UIKit;
 
 namespace DeliveriesApp.iOS
 {
     public partial class DeliveredViewController : UITableViewController
     {
+        List<Delivery> delivered;
+
         public DeliveredViewController (IntPtr handle) : base (handle)
         {
+        }
+
+        public override async void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            delivered = await Delivery.GetDelivered();
         }
     }
 }
--- DeliveriesApp.iOS/DeliveriesViewController.cs
+++ DeliveriesApp.iOS/DeliveriesViewController.cs
@@ -1,13 +1,26 @@
+using DeliveriesApp.Model;
 using Foundation;
 using System;
+using System.Collections.Generic;
 using UIKit;
 
 namespace DeliveriesApp.iOS
 {
     public partial class DeliveriesViewController : UITableViewController
     {
+
+        List<Delivery> deliveries;
+
         public DeliveriesViewController (IntPtr handle) : base (handle)
         {
+        }
+
+        // staple - override ViewDidLoad & therein load up members with data
+        public override async void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            deliveries = await Delivery.GetDeliveries();
         }
     }
 }
