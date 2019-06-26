7fffa9acbe3e709855d75d19118f5a73103fecd8
Peter N Jenkin <peternjenkin@gmail.com>
2019-06-26 21:31:26 +0100

    22-167 Not really working - TableViewCell with bodge to try workaround strange 'Unable to cast object of type 'System.RuntimeType' to type 'Mono.Debugger.Soft.TypeMirror'.' error, for iOS

--- DeliveriesApp.iOS/DeliveredViewController.cs
+++ DeliveriesApp.iOS/DeliveredViewController.cs
@@ -12,6 +12,8 @@ namespace DeliveriesApp.iOS
 
         public DeliveredViewController (IntPtr handle) : base (handle)
         {
+            // Because, ab initio, no deliveries will have been made, have to initialise the list so as to be non-null at least even if empty
+            delivered = new List<Delivery>();
         }
 
         public override async void ViewDidLoad()
@@ -19,6 +21,44 @@ namespace DeliveriesApp.iOS
             base.ViewDidLoad();
 
             delivered = await Delivery.GetDelivered();
+            TableView.ReloadData();      // after new data, refresh table (this method available because subclassing UI*Table*viewController)
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            // return base.RowsInSection(tableView, section);
+            return delivered.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+             return base.GetCell(tableView, indexPath);
+/*
+            var cell = tableView.DequeueReusableCell("deliveredCell");       // Identifier for cell in TableRowController
+
+            var deliveredValue = delivered[indexPath.Row];                  // get this row's value
+
+            cell.TextLabel.Text = deliveredValue.Name;
+
+            // cf DeliveriesAdaptor in Android
+            switch (deliveredValue.Status)
+            {
+                case 0:
+                    cell.DetailTextLabel.Text = "Awaiting delivery person";
+                    break;
+                case 1:
+                    cell.DetailTextLabel.Text = "Out for delivery";
+                    break;
+                case 2:
+                    cell.DetailTextLabel.Text = "Already delivered";
+                    break;
+                default:
+                    cell.DetailTextLabel.Text = "Delivery status unknown";
+                    break;
+            }
+
+            return cell;
+            */
         }
     }
 }
--- DeliveriesApp.iOS/DeliveriesViewController.cs
+++ DeliveriesApp.iOS/DeliveriesViewController.cs
@@ -20,7 +20,60 @@ namespace DeliveriesApp.iOS
         {
             base.ViewDidLoad();
 
-            deliveries = await Delivery.GetDeliveries();
+            var deliveriesData = await Delivery.GetDeliveries();
+
+            deliveries = deliveriesData;
+            ////deliveries = await Delivery.GetDeliveries();
+            TableView.ReloadData();      // after new data, refresh table (this method available because subclassing UI*Table*viewController)
+        }
+
+        // NB The 2 basic overrides for iOS cell population in TableViews: RowsInSection & GetCell
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            //return base.RowsInSection(tableView, section);       // boilerplate
+
+            // return 0 if deliveries is null
+            return deliveries?.Count ?? 0;
+            /*
+            return deliveries.Count;
+            //return deliveries.Count;            // as many rows as there are delivery records
+            */
+            // NB Only 1 section in this app’s case. Otherwise evaulate which section then calculate #rows.
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+
+            var cell = tableView.DequeueReusableCell("deliveryCell");     // recycle this 1 or more cells when fallen out of sight - Identifier value of cell in TableViewController
+
+            var deliveryValue = deliveries[indexPath.Row];                   // get this row's data for the cell
+
+            cell.TextLabel.Text = deliveryValue.Name;
+
+            // cf DeliveriesAdaptor in Android
+            switch (deliveryValue.Status)
+            {
+                case 0:
+                    cell.DetailTextLabel.Text = "Awaiting delivery person";
+                    break;
+                case 1:
+                    cell.DetailTextLabel.Text = "Out for delivery";
+                    break;
+                case 2:
+                    cell.DetailTextLabel.Text = "Already delivered";
+                    break;
+                default:
+                    cell.DetailTextLabel.Text = "Delivery status unknown";
+                    break;
+            }
+
+            // DetailTextLabel available because TableView cell style is 'Subtitle'
+
+            return cell;
+            
+            //return base.GetCell(tableView, indexPath);
+
         }
     }
 }
