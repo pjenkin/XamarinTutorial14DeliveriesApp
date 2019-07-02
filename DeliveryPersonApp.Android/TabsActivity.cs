695687f91652df5e33576fc5595af8f976c69742
Peter N Jenkin <peternjenkin@gmail.com>
2019-07-02 10:32:50 +0100

    25-177 Layouts & Activities & Fragments defined for Android

---
+++ DeliveryPersonApp.Android/TabsActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DeliveryPersonApp.Android
+{
+    [Activity(Label = "TabsActivity")]
+    public class TabsActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            // This activity is for displaying the delivered, delivering & waiting tabs/fragments
+            SetContentView(Resource.Layout.Tabs);
+        }
+    }
+}
