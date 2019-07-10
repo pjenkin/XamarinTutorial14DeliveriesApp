57546c31f2f146f91353e07ead5581fce8d82287
Peter N Jenkin <peternjenkin@gmail.com>
2019-07-10 13:35:00 +0100

    28-201 In-app purchase for iOS (bare bones thereof), with NuGet Plugin.InAppBilling package added

--- DeliveriesApp.iOS/MainTabBarController.cs
+++ DeliveriesApp.iOS/MainTabBarController.cs
@@ -1,4 +1,5 @@
 using Foundation;
+using Plugin.InAppBilling;
 using System;
 using UIKit;
 
@@ -8,6 +9,50 @@ namespace DeliveriesApp.iOS
     {
         public MainTabBarController (IntPtr handle) : base (handle)
         {
+            subscriptionBarButtonItem.Clicked += SubscriptionBarButtonItem_Clicked;
+        }
+
+
+        // In-App AppStore purchase
+        private async void SubscriptionBarButtonItem_Clicked(object sender, EventArgs e)
+        {
+            // throw new NotImplementedException();
+            try
+            {
+                var productId = "";     // if a ProductID has been declared online (App Store Connect: ProductID) , use here
+                var appStoreConnection = await CrossInAppBilling.Current.ConnectAsync();     // connect to AppStore using Plugin.InAppBilling 
+                // User will see AppStore dialogues according to available purchase type (e.g. subscription) set up previously
+
+                if (!appStoreConnection)
+                {
+                    return;
+                    // TODO show alert that purchase didn't happen (maybe due to network problems)
+                }
+
+                var purchase = await CrossInAppBilling.Current.PurchaseAsync(productId, Plugin.InAppBilling.Abstractions.ItemType.Subscription, "appPayLoadNotNeededYet");
+
+                if (purchase == null)       // if something went wrong so there d'seem to have been no purchase
+                {
+                    // TODO handle no purchase here
+                }
+                else
+                {
+                    // TODO handle results of purchase (unlocking, setting device memory UserDefaults &c) according to returned purchase value
+                    var id = purchase.Id;
+                    var token = purchase.PurchaseToken;
+                    var autoRenewing = purchase.AutoRenewing;
+                    var state = purchase.State;         // state could be cancelled, free trial, deferred, paymentpending, failed, uknown, restored, purchased ...
+                }
+            }
+            catch (Exception exc)
+            {
+                // TODO: handle untoward errors here
+            }
+
+        finally
+            {
+                await CrossInAppBilling.Current.DisconnectAsync();      // important to disconnect from App Store correctly
+            }
         }
 
         public override void ViewDidAppear(bool animated)
--- DeliveriesApp.iOS/MainTabBarController.designer.cs
+++ DeliveriesApp.iOS/MainTabBarController.designer.cs
@@ -14,8 +14,16 @@ namespace DeliveriesApp.iOS
     [Register ("MainTabBarController")]
     partial class MainTabBarController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIBarButtonItem subscriptionBarButtonItem { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (subscriptionBarButtonItem != null) {
+                subscriptionBarButtonItem.Dispose ();
+                subscriptionBarButtonItem = null;
+            }
         }
     }
 }
